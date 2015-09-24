ac89700e8cd3b8748f903d4fb1b586b16a58fa68
genesixx <erwanleboucher@gmail.com>
2015-09-24 21:47:01 +0200

    g4: fix derp from cherry-pick
    this will need to be clean later

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -11,7 +11,6 @@ LOCAL_SRC_FILES    := etc/fstab.qcom
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
-
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.rc
 LOCAL_MODULE_TAGS  := optional eng
@@ -46,7 +45,6 @@ LOCAL_SRC_FILES    := etc/init.qcom.bt.sh
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-<<<<<<< HEAD
 LOCAL_MODULE       := init.qcom.coex.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
@@ -58,13 +56,6 @@ LOCAL_MODULE       := init.qcom.fm.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.fm.sh
-=======
-LOCAL_MODULE            := init.qcom.usb.rc
-LOCAL_MODULE_TAGS       := optional eng
-LOCAL_MODULE_CLASS      := ETC
-LOCAL_SRC_FILES         := etc/init.qcom.usb.rc
-LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
->>>>>>> ff47093... g4: remove init.qcom.sh
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
